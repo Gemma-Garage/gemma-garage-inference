02b6738ddaddcdb1b43b6bea70166ea7dc09f4d6
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2025-07-24 21:39:34 -0300

    fixing CORS issue

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from endpoints import inference
 app = FastAPI(title="LLM Garage API - Inference Engine")
 
 origins = [
-    "http://localhost:3000",  # your React app's origin
+    "http://localhost:3000",
+    "http://localhost",
+    "http://127.0.0.1:3000",
+    "https://gemma-garage.web.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=origins,  # Only allow specific origins
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 #inference endpoint
-app.include_router(inference.router, prefix="/inference", tags=["Inference"])
-
+app.include_router(inference.router, tags=["Inference"])
 
 if __name__ == "__main__":
     import uvicorn
